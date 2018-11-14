559d67124b73f4c7564b350ca62412f9867583e1
Zoltan Giber <zgiber@gmail.com>
2018-11-14 12:20:03 +0000

    recompile authz pb.go

--- contract/authz.pb.go
+++ contract/authz.pb.go
@@ -6,10 +6,11 @@ package contract
 import (
 	context "context"
 	fmt "fmt"
+	math "math"
+
 	proto "github.com/golang/protobuf/proto"
 	_ "google.golang.org/genproto/googleapis/api/annotations"
 	grpc "google.golang.org/grpc"
-	math "math"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
